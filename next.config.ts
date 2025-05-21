e4ae16ada5d0a33464288397a67a9b72a0df441b
Hleb Kushniarou <h.kushniarou@atom.corp>
2025-05-21 19:30:42 +0300

    updated tag shrink and images remote url

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL('http://localhost:3001/**')],
+    remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_API_URL}/**`)],
   },
 };
 
